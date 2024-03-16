6b944b76e4469f3430aaf3ea7fb810040f25d0e9
JahirPendhari <jahirpp1999@gmail.com>
2024-03-16 21:02:57 +0530

    backend Done for Todo App

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const  express = require('express');
 const { connection } = require('./db')
-const { todoRoutes } = require('./routes/todo.route')
+const { todoRoutes } = require('./routes/todo.route');
+const cors = require('cors')
 
 require('dotenv').config();
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/todos', todoRoutes)
 
