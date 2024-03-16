95ef7bd25718f60479c0a3f48ff429c24f9c6fce
JahirPendhari <jahirpp1999@gmail.com>
2024-03-16 20:12:55 +0530

    backend Done for Todo App

--- index.ts
+++ index.ts
@@ -7,9 +7,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/todos', todoRoutes)
-app.get("/",(req:any , res:any)=>{
-    res.send("Welcome to Todo App by Jahir Pendhari")
-})
 
 app.listen(process.env.PORT , async()=>{
     try{
